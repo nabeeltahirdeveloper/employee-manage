0e24185ef048fdeb5910c467346ed8c15de7df91
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-04-06 05:22:54 +0500

    v2

---
+++ src/app/Employee.ts
@@ -0,0 +1,8 @@
+export interface Employee {
+    name: string;
+    email: string;
+    phone: number;
+    address: string;
+    salary: number;
+    position: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddEmployeeComponent } from './myComponents/add-employee/add-employee.component';
+import { EmployeeManageComponent } from './myComponents/employee-manage/employee-manage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddEmployeeComponent,
+    EmployeeManageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
